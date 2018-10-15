23af5a7349865911f56fb5d9dcfc30dd809fcba1
j.c.sackett <j.c.sackett@gmail.com>
2018-10-15 14:08:11 -0400

    Makes sure apparmor isn't disabled
    Apparmor is necessary for the lxc user to use snap binaries, e.g. kubectl

--- files/setup-systemd.sh
+++ files/setup-systemd.sh
@@ -3,6 +3,7 @@
 # Disable systemd services that are not required to start on boot.
 
 systemctl -a | awk '{print $1}' | grep \.service | \
+    grep -v apparmor | \
     grep -v cloud-init | \
     grep -v dbus | \
     grep -v network | \
